549b8d0c4eb167ffe1532bcda235cd9a472381c8
Heval Demirel <hevalzzz@hotmail.com>
2024-04-07 18:29:21 +0200

    Implementerat Routes och konfig

--- havves/src/app/app.component.ts
+++ havves/src/app/app.component.ts
@@ -1,24 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { NavbarComponent } from './pages/navbar/navbar.component';
-import { HttpClientModule } from '@angular/common/http';
+
 import { LoginComponent } from './pages/login/login.component';
 import { FooterComponent } from './pages/footer/footer.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { RecipeComponent } from './pages/recipe/recipe.component';
 import { SearchComponent } from './pages/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, NavbarComponent, HttpClientModule, RouterModule, LoginComponent, FooterComponent, RegisterComponent, RecipeComponent, SearchComponent],
+  imports: [RouterLinkActive,RouterOutlet, RouterLink, NavbarComponent, HttpClientModule, RouterModule, LoginComponent, FooterComponent, RegisterComponent, RecipeComponent, SearchComponent,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
   title = 'havves';
-  ngOnInit(): void {
-    initFlowbite();
-  }
+
 }
--- havves/src/app/app.routes.ts
+++ havves/src/app/app.routes.ts
@@ -3,18 +3,22 @@ import { RecipeComponent } from './pages/recipe/recipe.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
-import { MainComponent } from './pages/main/main.component';
+import { FooterComponent } from './pages/footer/footer.component';
 import { DessertComponent } from './pages/dessert/dessert.component';
 import { StarterComponent } from './pages/starter/starter.component';
 import { SearchComponent } from './pages/search/search.component';
+import { MainsComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+
     { path: 'recipe/:id', component: RecipeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'home', component: HomeComponent },
-    { path: 'main', component: MainComponent },
+    { path: 'main', component: MainsComponent},
     { path: 'dessert', component: DessertComponent },
     { path: 'starter', component: StarterComponent },
-    { path: 'search', component: SearchComponent },
+    { path: '', redirectTo: '/search', pathMatch: 'full' },
+
 ];
