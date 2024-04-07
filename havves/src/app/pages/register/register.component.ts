240fd29f8a8e1b814c3eef51a4ff6a51a4b31759
Heval Demirel <hevalzzz@hotmail.com>
2024-04-07 18:32:03 +0200

    Registrering, styling , komponent

--- havves/src/app/pages/register/register.component.ts
+++ havves/src/app/pages/register/register.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { Register } from '../models/Register.model';
+import { AuthService } from '../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
-  templateUrl: './register.component.html',
+  imports: [FormsModule,HttpClientModule,RouterLink],
+  templateUrl:'./register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerdetails: Register;
+  errorMessage: string = ''; 
 
+
+  constructor(private auth: AuthService, private router: Router) { 
+    this.registerdetails = new Register();
+  }
+
+  register() {
+    this.auth.registerUser(this.registerdetails).subscribe({
+      next: (response) => {
+        console.log('User registered successfully', response);
+        alert('Your account has been created successfully. Please login.');
+
+        
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.error('Registration error', error);
+        this.errorMessage='Registration failed. Please try again.'
+      }
+    });
+  }
 }
