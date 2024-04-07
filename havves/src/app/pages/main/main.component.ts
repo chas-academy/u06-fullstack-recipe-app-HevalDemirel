813364f5f39faf4d2399ac73392db24145c04643
Heval Demirel <hevalzzz@hotmail.com>
2024-04-07 18:31:04 +0200

    Main komponente implementering

--- havves/src/app/pages/main/main.component.ts
+++ havves/src/app/pages/main/main.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
-import { RecipeService } from '../services/recipe.service';
 import { Router } from '@angular/router';
 import { SearchComponent } from '../search/search.component';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
-  selector: 'app-main',
+  selector: 'app-mains',
   standalone: true,
   imports: [SearchComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+
+export class MainsComponent {
   dishType = "Main course";
   recipes?: any;
   constructor(private recipeService: RecipeService, private router: Router) { }
@@ -18,7 +19,7 @@ export class MainComponent {
     this.recipes = recipes
   }
   goToDetails(recipe: any) {
-    let startIndex = recipe.self.indexOf("/v2/") + 4;
+    let startIndex = recipe.self.indexOf("/v2/") + 4; 
     let endIndex = recipe.self.indexOf("?");
     let extractedId = recipe.self.substring(startIndex, endIndex);
     console.log(extractedId);
