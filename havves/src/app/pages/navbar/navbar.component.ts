9c15bf5e0c24221b469813fed1efa2fbb79931cd
Heval Demirel <hevalzzz@hotmail.com>
2024-04-07 18:31:42 +0200

    + navbar , styling, komponent, anuglar material

--- havves/src/app/pages/navbar/navbar.component.ts
+++ havves/src/app/pages/navbar/navbar.component.ts
@@ -9,14 +9,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { Token } from '@angular/compiler';
-import { BrowserModule } from '@angular/platform-browser';
 import { AuthService } from '../services/auth.service';
-import { state } from '@angular/animations';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css',
+  styleUrl: './navbar.component.scss',
   standalone: true,
   imports: [
     MatToolbarModule,
@@ -31,38 +29,30 @@ import { state } from '@angular/animations';
   ],
 })
 export class NavbarComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
+ 
   private breakpointObserver = inject(BreakpointObserver);
   private authservice = inject(AuthService);
+
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
       shareReplay()
     );
-    // ngOnInit(): void {
-    // this.authservice.getTokenState().subscribe(state => {
-    //   this.loggedIn = state
-    // })
-    // }
+    ngOnInit(): void {
+    this.authservice.getTokenState().subscribe(state => {
+      this.loggedIn = state
+    })
+    }
+    
   loggedIn: boolean = false
-  // logout() {
-  //   const token = localStorage.getItem('token');
-  //   this.authservice.postLogout(token).subscribe((res) => {
-  //     localStorage.removeItem('token');
-  //     this.authservice.updateTokenState()
-  //     console.log(res);
-  //   });
-  // }
+
+  logout() {
+    const token = localStorage.getItem('token');
+    this.authservice.postLogout(token).subscribe((res) => {
+      localStorage.removeItem('token');
+      this.authservice.updateTokenState()
+      console.log(res);
+    });
+  }
 }
-
-
-
-
-
-
-
-
-
